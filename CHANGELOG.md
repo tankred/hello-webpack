11c92670172459955e345bd4e5a1686442f34331
Kurt Duyck <kurt.duyck@vives.be>
2020-07-06 14:19:44 +0200

    Build to distribution dir
    	modified:   .gitignore
    	modified:   CHANGELOG.md
    	modified:   package.json
    	modified:   webpack.config.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.3] - 2020-07-06
+### Changed
+- Use dist dir
+
 ## [0.1.2] - 2020-06-24
 ### Changed
 - Add run dev
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "development", // could be "production" as well
   entry: './src/main.js', 
   output: {
-    path: path.resolve(__dirname, 'public'), 
+    path: path.resolve(__dirname, 'dist'), 
     filename: 'bundle.js' 
   }
 };
